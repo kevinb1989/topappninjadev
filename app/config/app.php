c61284047a8723fedf9edce8ea3c8213f0c3ac4a
kevinb1989 <hoanglongbui89@yahoo.com.vn>
2015-11-11 22:16:12 +1100

    remove CashierServiceProvider

--- app/config/app.php
+++ app/config/app.php
@@ -121,7 +121,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
-		'Laravel\Cashier\CashierServiceProvider',
+		//'Laravel\Cashier\CashierServiceProvider',
 		'Barryvdh\Debugbar\ServiceProvider',
 		'TopAppNinja\Repositories\RepositoryServiceProvider'
 
